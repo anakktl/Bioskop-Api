8a061e0378b3e370aa731562a8f4256d09f6ee7c
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-03-23 11:25:27 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,13 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    config = {
-        status: true,
-        result: {
-            "baca docs nya di github.com/Zhirrr"
-        }
-    }
-    res.json(config)
+    res.json({ error: "Silahkan baca dokumentasi di https://github.com/devnazir/api-lk21/#readme" })
 })
 
 

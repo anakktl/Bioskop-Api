72cf289417df5e8c5b89eacb60ebeefeecceea7f
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-03-23 11:25:49 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ error: "Silahkan baca dokumentasi di https://github.com/devnazir/api-lk21/#readme" })
+    res.json({ error: "Silahkan baca dokumentasi di https://github.com/Zhirrr/Bioskop-Api" })
 })
 
 
